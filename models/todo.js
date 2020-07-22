1ddf49050f0ae92d4b6d46efe2c4b38a887a6f26
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 19:29:45 +0600

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+            const mongoose = require('mongoose')
+            const Schema = mongoose.Schema
 
-//Create Schema
 
-const todoSchema = new Schema({
-    name : {
-        type : String,
-        required : true
-    },
-    added_date : {
-        type : Date,
-        default : Date.now
-    }
-})
+                const todoSchema = new Schema({
+                    name : {
+                        type : String,
+                        required : true
+                    },
+            added_date : {
+                type : Date,
+                default : Date.now
+            }
+        })
 
-// Export Schema
 
-module.exports = Todo = mongoose.model('todo', todoSchema)
+
+                    module.exports = Todo = mongoose.model('todo', todoSchema)
